50054d5f92fec4a6e0783115cdbf047c02bcfa80
Michael Lehmann <lehmamic@bluewin.ch>
2014-11-30 22:31:06 +0100

    basic gulp-inject configuration

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,34 @@
 var gulp = require('gulp');
+var inject = require("gulp-inject");
+var bowerFiles = require('main-bower-files');
+var es = require('event-stream');
+var angularFilesort = require('gulp-angular-filesort');
 
 gulp.task('default', function() {
     // place code for your default task here
+});
+
+gulp.task('index', function () {
+    //var target = gulp.src('./public/index.html');
+    // It's not necessary to read the files (will speed up things), we're only after their paths:
+    //var sources = gulp.src(['./src/**/*.js', './src/**/*.css'], {read: false});
+
+    var libFiles = gulp.src(bowerFiles(), {read: false})
+        .pipe(angularFilesort());
+    var sources = gulp.src('./public/app/**/*.js', {read: false})
+        .pipe(angularFilesort());
+    var assets = gulp.src('./public/assets/**/*.css');
+
+    return gulp.src('./public/index.html')
+        .pipe(inject(libFiles), {name: 'bower'})
+        .pipe(inject(es.merge(sources, assets)))
+        .pipe(gulp.dest('./public'));
+});
+
+gulp.task('watch', function() {
+    gulp.watch(['./public/**/*.js', './public/**/*.css', './public/**/*.html'], ['index']);
+});
+
+gulp.task('build', function() {
+
 });
---
+++ public/app/app.js
@@ -0,0 +1,7 @@
+/**
+ * Created by lehmamic on 30/11/14.
+ */
+(function () {
+    'use strict';
+
+})();
