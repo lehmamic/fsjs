8d1f9e7f8a8acc3d43e28b8134e1d06e2684726b
lehmamic <leh@Zuehlke.com>
2014-12-09 17:14:22 +0100

    gulp inject is working now on windows

--- gulpfile.js
+++ gulpfile.js
@@ -11,16 +11,15 @@ var imagemin = require('gulp-imagemin');
 var pngquant = require('imagemin-pngquant');
 var rev = require('gulp-rev');
 var clean = require('gulp-clean');
-var using = require('gulp-using');
 var debug = require('gulp-debug');
-var path = require('path');
+var path = require('canonical-path');
 var mocha = require('gulp-mocha');
 
 var paths = {
   index: './public/index.html',
   html: './public/**/*.html',
   scripts: './public/app/**/*.js',
-  assets: './public/assets/**/*.css',
+  styles: './public/assets/**/*.css',
   images: '/public/images/**/*.{png,jpg,jpeg}',
   bower: './bower.json',
   nodetests: './test/unit/**/*.js',
@@ -29,64 +28,59 @@ var paths = {
 };
 
 gulp.task('inject', function () {
-    var components = gulp.src(bowerFiles(), {read: false})
-        .pipe(angularFilesort())
-        .pipe(debug({verbose: false}));
+    var components = gulp.src(bowerFiles());
 
-	var scripts = gulp.src(paths.scripts, {read: false})
-        .pipe(angularFilesort())
-        .pipe(debug({verbose: false}));
-		
-    var assets = gulp.src(paths.assets, {read: false})
-        .pipe(debug({verbose: false}));
+	var scripts = gulp.src(paths.scripts)
+        .pipe(angularFilesort());
 
-    return gulp.src(paths.index)
-        .pipe(debug({verbose: false}))
+    var styles = gulp.src(paths.styles);
+
+    return gulp.src('./public/index.html')
+        .pipe(inject(es.merge(scripts, styles), {relative: true}))
         .pipe(inject(components, {name: 'bower', relative: true}))
-        .pipe(inject(es.merge(scripts, assets), {relative: true}))
-        .pipe(gulp.dest(paths.app));
+        .pipe(gulp.dest('./public'));
 });
 
-gulp.task('usemin', function() {
-  return gulp.src(paths.html)
-    .pipe(usemin({
-      css: [minifyCss(), 'concat'],
-      html: [minifyHtml({empty: true})],
-      js: [uglify(), rev()]
-    }))
-    .pipe(gulp.dest(paths.dist));
-});
+//gulp.task('usemin', function() {
+//  return gulp.src(paths.html)
+//    .pipe(usemin({
+//      css: [minifyCss(), 'concat'],
+//      html: [minifyHtml({empty: true})],
+//      js: [uglify(), rev()]
+//    }))
+//    .pipe(gulp.dest(paths.dist));
+//});
 
-gulp.task('imagemin', function() {
-    return gulp.src(paths.images)
-        .pipe(imagemin({
-            progressive: true,
-            svgoPlugins: [{removeViewBox: false}],
-            use: [pngquant()]
-        }))
-        .pipe(gulp.dest(path.join(paths.dist, 'images')));
-});
-
-gulp.task('watch', function() {
-  gulp.watch([paths.scripts, paths.assets, paths.bower], ['inject']);
-});
-
-gulp.task('clean', function() {
-  return gulp.src(paths.dist, {read: false})
-      .pipe(clean());
-});
-
-gulp.task('copy', function() {
-
-});
-
-gulp.task('test', function() {
-    return gulp.src(paths.nodetests, {read: false})
-        .pipe(mocha({reporter: 'spec'}));
-});
-
-gulp.task('build', ['clean', 'inject', 'usemin', 'imagemin', 'copy']);
-
-gulp.task('default', ['build'], function() {
-    // place code for your default task here
-});
+//gulp.task('imagemin', function() {
+//    return gulp.src(paths.images)
+//        .pipe(imagemin({
+//            progressive: true,
+//            svgoPlugins: [{removeViewBox: false}],
+//            use: [pngquant()]
+//        }))
+//        .pipe(gulp.dest(path.join(paths.dist, 'images')));
+//});
+//
+//gulp.task('watch', function() {
+//  gulp.watch([paths.scripts, paths.assets, paths.bower], ['inject']);
+//});
+//
+//gulp.task('clean', function() {
+//  return gulp.src(paths.dist, {read: false})
+//      .pipe(clean());
+//});
+//
+//gulp.task('copy', function() {
+//
+//});
+//
+//gulp.task('test', function() {
+//    return gulp.src(paths.nodetests, {read: false})
+//        .pipe(mocha({reporter: 'spec'}));
+//});
+//
+//gulp.task('build', ['clean', 'inject', 'usemin', 'imagemin', 'copy']);
+//
+//gulp.task('default', ['build'], function() {
+//    // place code for your default task here
+//});
